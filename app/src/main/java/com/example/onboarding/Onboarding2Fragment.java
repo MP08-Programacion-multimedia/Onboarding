de5e15ad01c5dc2c00830dfbcb040107e0aac033
rogerroque <reroqueo01@elpuig.xeill.net>
2020-10-19 22:06:48 +0200

    Resubida despues de configurar lo de las organizaciones para cada proyecto

---
+++ app/src/main/java/com/example/onboarding/Onboarding2Fragment.java
@@ -0,0 +1,46 @@
+package com.example.onboarding;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.onboarding.databinding.FragmentOnboarding1Binding;
+import com.example.onboarding.databinding.FragmentOnboarding2Binding;
+
+public class Onboarding2Fragment extends Fragment {
+
+    FragmentOnboarding2Binding binding;
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentOnboarding2Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        binding.botonFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_onboarding2Fragment_to_homeFragment);
+            }
+        });
+    }
+
+}
